dc9e3f66205cf4dc3b405a18e4417358912ce502
Gagan kumar indoriya <panaceahelporiginal@gmail.com>
2019-12-19 23:38:23 +0530

    Add Calc

--- src/Calc.java
+++ src/Calc.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
+//Simple Calculator Program using switch
 public class Calc {
+    static int a, b, c;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter First Number");
+        a = sc.nextInt();
+        System.out.println("Please Enter Second Number");
+        b = sc.nextInt();
+        System.out.println("Please Enter +,-,*,/");
+        String operator = sc.next();
+        char op = operator.charAt(0);
+        switch (op) {
+            case '+':
+                c = a + b;
+                break;
+            case '-':
+                c = a + b;
+                break;
+            case '*':
+                c = a + b;
+                break;
+            case '/':
+                c = a + b;
+                break;
+        }
+        System.out.println("Answer is :"+c);
+    }
+
 }
+
